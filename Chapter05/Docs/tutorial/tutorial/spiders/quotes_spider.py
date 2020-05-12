37f67c8b5b38e258235c87d780eb8fc24593ae9c
PhlipTung <philip.dongfei@gmail.com>
2020-05-12 16:30:43 +0800

    scropy tutorial: Storing the scraped data

---
+++ Chapter05/Docs/tutorial/tutorial/spiders/quotes_spider.py
@@ -0,0 +1,29 @@
+import scrapy
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+
+    # a shortcut to the start_requests method
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+        'http://quotes.toscrape.com/page/2/',
+    ]
+    '''
+
+    def start_requests(self):
+        urls = [
+            'http://quotes.toscrape.com/page/1/',
+            'http://quotes.toscrape.com/page/2/',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+    '''
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
+
+

748880eef2497593910959ee8a3d6f84163c5ea3
PhlipTung <philip.dongfei@gmail.com>
2020-06-15 10:49:42 +0800

    Chapter 16: Web Crawling in Parallel

---
+++ Chapter16/multiprocess_example.py
@@ -0,0 +1,27 @@
+from multiprocessing import Process
+import time
+import os
+
+
+def print_time(threadName, delay, iterations):
+    pid = os.getpid()
+    start = int(time.time())
+    for i in range(0, iterations):
+        time.sleep(delay)
+        seconds_elapsed = str(int(time.time()) - start)
+        print((pid,threadName) if threadName else seconds_elapsed)
+
+processes = []
+processes.append(Process(target=print_time, args=(None, 1, 100)))
+processes.append(Process(target=print_time, args=('Fizz', 3, 33)))
+processes.append(Process(target=print_time, args=('Buzz', 5, 20)))
+
+
+for p in processes:
+    p.start()
+
+for p in processes:
+    p.join()
+
+print("Program complete")
+
---
+++ Chapter16/multithreaded_queue.py
@@ -0,0 +1,61 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+import random
+import _thread
+from queue import Queue
+import time
+import pymysql
+
+def storage(queue):
+    conn = pymysql.connect(host='127.0.0.1',
+                       user='philip', passwd='75356072Philip&%#%^)&@',
+                       db='mysql', charset='utf8' )
+    cur = conn.cursor()
+    cur.execute('USE wiki_threads')
+    while 1:
+        if not queue.empty():
+            article = queue.get()
+            cur.execute('SELECT * FROM pages WHERE path = %s', (article["path"]))
+            if cur.rowcount == 0:
+                print("Storing article {}".format(article["title"]))
+                cur.execute('INSERT INTO pages (title, path) VALUES (%s, %s)', (article["title"], article["path"]))
+                conn.commit()
+            else:
+                print("Article already exists: {}".format(Article['title']))
+
+
+visited = []
+def getLinks(thread_name, bsObj):
+    print('Getting links in {}'.format(thread_name))
+    links = bsObj.find('div', {'id':'bodyContent'}).find_all('a', href=re.compile('^(/wiki/)((?!:).)*$'))
+    return [link for link in links if link not in visited]
+
+def scrape_article(thread_name, path, queue):
+    visited.append(path)
+    html = urlopen('http://en.wikipedia.org{}'.format(path))
+    time.sleep(5)
+    bsObj = BeautifulSoup(html, 'html.parse')
+    title = bsObj.find('h1').get_text()
+    print('Added {} for storage in thread {}'.format(title, thread_name))
+    queue.put({"title":title, "path":path}) # put {dict}
+    links = getLinks(thread_name, bsObj)
+    if len(links) > 0:
+        newArticle = links[random.randint(0, len(links)-1)].attrs['href']
+        scrape_article(thread_name, newArticle, queue)
+
+if __name__ == '__main__':
+    queue = Queue()
+    try:
+        _thread.start_new_thread(scrape_article, ('Thread 1', '/wiki/Kevin_Bacon', queue, ))
+        _thread.start_new_thread(scrape_article, ('Thread 2', '/wiki/Monty_Python', queue, ))
+        _thread.start_new_thread(storage, (queue,))
+    except:
+        print("Error: unable to start threads")
+
+    while 1:
+        pass
+
+
+
+
